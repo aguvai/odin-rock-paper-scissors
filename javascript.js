e08d0668129fa5dac08f6147491fe90871e8bf29
aguvai <145935152+aguvai@users.noreply.github.com>
2025-05-24 18:29:25 -0400

    Add the logic to get the human choice
    getHumanChoice() prompts the human player to ask them what move they
    would like to play. This is needed to create the gameplay loop.

--- javascript.js
+++ javascript.js
@@ -2,3 +2,9 @@ function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function getHumanChoice() {
+    return prompt("Rock, paper, or scissors?");
+}
+
+console.log(getHumanChoice())
