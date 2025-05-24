81bf336540e233ead23de36fe0e9385885677d42
aguvai <145935152+aguvai@users.noreply.github.com>
2025-05-24 19:07:54 -0400

    Add round logic framework
    Established a skeleton for the playRound function that will enable
    the program to process the player's input and measure it against
    the computer's choice to calculate round outcomes.
    
    sendResultMessage simplifies result logging by reducing code repetition.
    
    Added "let" before global variable declarations for best practice.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
+
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
@@ -7,5 +11,29 @@ function getHumanChoice() {
     return prompt("Rock, paper, or scissors?");
 }
 
-humanScore = 0;
-computerScore = 0;
+function sendResultMessage(result, humanChoice, computerChoice) {
+    if (result == "humanWon") {
+        console.log(`You won! ${humanChoice} beats ${computerChoice}.`)
+    } else if (result == "humanLost") {
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}.`)
+    } else if (result == "tie") {
+        console.log(`It's a tie! The computer and the human both chose ${humanChoice}.`)
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    switch(humanChoice.toLowerCase()) {
+        case "rock":
+            sendResultMessage("humanWon", humanChoice, computerChoice)
+            break;
+        case "paper":
+            sendResultMessage("humanLost", humanChoice, computerChoice)
+            break;
+        case "scissors":
+            sendResultMessage("tie", humanChoice, computerChoice)
+            break;
+    }
+
+}
+
+playRound(getHumanChoice(), getComputerChoice())
